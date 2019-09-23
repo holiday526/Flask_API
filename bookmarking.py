58f7c49740b08768001c374ede79962c37d2b41d
holiday526 <taylorchan526@gmail.com>
2019-09-23 23:24:48 +0800

    bookmarking.py updated

--- bookmarking.py
+++ bookmarking.py
@@ -1,49 +1,276 @@
 from flask import Flask
 from flask import json
-from flask import jsonify
-from flask import Response
+from flask import request
+import sqlite3
 
 app = Flask(__name__)
 
+ok_code, created_code, no_content_code, bad_request_code, not_found_code, internal_server_error_code = 200, 201, 204, 400, 404, 500
 
-@app.route('/')
-def home():
-    return 'hello world'
+msg_user_already_exists = 'User already exists'
+msg_malfunction_attri_exists = 'Malfunction key exists'
+msg_user_not_found = 'User not found'
+
+db_file = 'bookmarks.db'
 
 
+class BadRequestException(Exception):
+    """Bad Request"""
+    pass
+
+
+class MalfunctionException(Exception):
+    """Malfunction Exception"""
+    pass
+
+
+def response(data, status_code):
+    r = app.response_class(
+        response=json.dumps(data, sort_keys=False),
+        status=int(status_code),
+        mimetype='application/json'
+    )
+    return r
+
+
+def create_connection(db_filename):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_filename)
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return "<h1>500</h1><p>Internal server error</p>", 500
+
+
+# show all users
 @app.route('/bookmarking/users', methods=['GET'])
 def users_index():
     # response body
     # All users(ID, name) in JSON(Sorted by ascending user ID(s))
-    users = [{'abc': 'test'}]
-    response = app.response_class(
-        response=json.dumps(users),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    conn = create_connection(db_file)
+
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users;')
+    rows = cur.fetchall()
+
+    all_users = []
+    for row in rows:
+        all_users.append(dict(row))
+
+    users_count = len(rows)
+
+    return response({'count': users_count, 'users': all_users}, created_code)
 
 
+# create user
 @app.route('/bookmarking/<user_id>', methods=['POST'])
 def users_create(user_id):
-    """
-    Request Body
-    new user/user's id and name in JSON format
-    """
-    """
-    Response Body
-    new user's id and name id in JSON format (if the operation is successful)
-    Reason for the error (when operation is not successful)
-    """
-    """
-    Response Code
-    201 (Created)
-    400 (Bad Request): One/more of the user(s)
-    500 (Internal Server Error): When the input is malformed or request that may raise exceptions
-    """
-    #
-    return
+    if request.method == 'POST':
+        try:
+            # If more than two attr
+            if len(request.get_json()) is not 2:
+                raise MalfunctionException
+
+            uid = request.json['user_id']
+
+            # If the user_id in URI does not match with the user_id in json
+            if str(uid) != str(user_id):
+                raise MalfunctionException
+
+            user_name = request.json['user_name']
+
+        except MalfunctionException as err:
+            # malfunction key attr
+            # msg = [{'message': msg_user_already_exists}]
+            # return response({'reason': msg}, internal_server_error_code)
+            return internal_server_error(internal_server_error_code)
+
+        conn = create_connection(db_file)
+        cur = conn.cursor()
+
+        try:
+            cur.execute('INSERT INTO users VALUES (?, ?);', (uid, user_name))
+        except sqlite3.IntegrityError:
+            msg = [{'message': msg_user_already_exists}]
+            return response({'reason': msg}, bad_request_code)
+
+        conn.commit()
+        conn.close()
+
+        return response({'status': created_code}, created_code)
+
+
+# delete user
+@app.route('/bookmarking/<user_id>', methods=['DELETE'])
+def users_delete(user_id):
+    conn = create_connection(db_file)
+    cur = conn.cursor()
+
+    cur.execute('DELETE FROM users WHERE user_id=?', (user_id,))
+
+    conn.commit()
+    conn.close()
+
+    if cur.rowcount is 1:
+        return response({'status': no_content_code}, no_content_code)
+    elif cur.rowcount is 0:
+        msg = [{'message': msg_user_not_found}]
+        return response({'reason': msg}, not_found_code)
+
+
+# bookmark crud
+# bookmark index
+@app.route('/bookmarking/bookmarks', methods=['GET'])
+def bookmarks_index():
+    data_key = {}
+    accepted_key = ['tag', 'count', 'offset']
+
+    # TODO: Check if the tag more than one tag -> split(',')
+    tag = request.args.get('tag')
+    count = request.args.get('count')
+    offset = request.args.get('offset')
+
+    try:
+        for x in list(request.args):
+            if x not in accepted_key:
+                raise MalfunctionException
+    except MalfunctionException as err:
+        # return response({'status': bad_request_code}, bad_request_code)
+        return internal_server_error(internal_server_error_code)
+
+    conn = create_connection(db_file)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM bookmarks"
+
+    tuple_value = ()
+
+    # TODO: do a foreach for the multiple tags, using WHERE AND LIKE
+    if tag:
+        tags = str(tag).split(",")
+        if tags > 1:
+            first = True
+            for x in tags:
+                if first:
+                    tuple_value = tuple_value + ("%" + str(x) + "%",)
+                    sql = sql + "WHERE tags LIKE ?"
+                    first = False
+                else:
+                    tuple_value = tuple_value + ("%" + str(x) + "%",)
+                    sql = sql + "AND tags LIKE ?"
+        else:
+            tuple_value = tuple_value + ("%" + str(tag) + "%",)
+            sql = sql + " WHERE tags LIKE ?"
+
+    sql = sql + " ORDER BY url ASC, user_id ASC"
+
+    if count:
+        tuple_value = tuple_value + (count,)
+        sql = sql + ' LIMIT ?'
+
+    if offset:
+        tuple_value = tuple_value + (offset,)
+        sql = sql + ' OFFSET ?'
+
+    cur.execute(sql, tuple_value)
+    rows = cur.fetchall()
+
+    conn.close()
+
+    result = []
+
+    for row in rows:
+        result.append(dict(row))
+
+    row_count = len(rows)
+
+    return response({'count': row_count, 'bookmarks': result}, ok_code)
+
+
+@app.route('/bookmarking/bookmarks/<user_id>', methods=['GET'])
+def bookmarks_show(user_id):
+    # TODO: check the user_id first, if there is no user_id return 404
+    conn = create_connection(db_file)
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users WHERE user_id=?', (user_id,))
+
+    conn.commit()
+    conn.close()
+
+    if cur.rowcount is 0:
+        msg = [{'message': msg_user_not_found}]
+        return response({'reason': msg}, not_found_code)
+
+    accepted_key = ['tag', 'count', 'offset']
+
+    tag = request.args.get('tag')
+    count = request.args.get('count')
+    offset = request.args.get('offset')
+
+    try:
+        for x in list(request.args):
+            if x not in accepted_key:
+                raise MalfunctionException
+    except MalfunctionException as err:
+        # return response({'status': bad_request_code}, bad_request_code)
+        return internal_server_error(internal_server_error_code)
+
+    conn = create_connection(db_file)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM bookmarks"
+
+    tuple_value = ()
+
+    tuple_value = tuple_value + ("'" + str(user_id) + "'",)
+    sql = sql + "WHERE user_id = ?"
+
+    if tag:
+        tags = str(tag).split(",")
+        for x in tags:
+            tuple_value = tuple_value + ("%" + str(x) + "%",)
+            sql = sql + "AND tags LIKE ?"
+
+    sql = sql + " ORDER BY url ASC"
+
+    if count:
+        tuple_value = tuple_value + (count,)
+        sql = sql + ' LIMIT ?'
+
+    if offset:
+        tuple_value = tuple_value + (offset,)
+        sql = sql + ' OFFSET ?'
+
+    if tag:
+        tuple_value = tuple_value + ("%" + str(tag) + "%",)
+
+    cur.execute(sql, tuple_value)
+    rows = cur.fetchall()
+
+    conn.close()
+
+    result = []
+
+    for row in rows:
+        result.append(dict(row))
+
+    row_count = len(rows)
+
+    if row_count:
+        return response({'count': row_count, 'bookmarks': result}, ok_code)
+    else:
+        return response({}, )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
